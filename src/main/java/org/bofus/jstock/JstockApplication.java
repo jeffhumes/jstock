237d5868fe9ba466d9092778e87d20d409b59ba8
Jeff Humes <jeffitmind@gmail.com>
2024-10-21 14:34:08 -0500

    initial code addition

---
+++ src/main/java/org/bofus/jstock/JstockApplication.java
@@ -0,0 +1,13 @@
+package org.bofus.jstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JstockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/bofus/jstock/JstockApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bofus.jstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
