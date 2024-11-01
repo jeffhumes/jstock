6fcb44fda45d69cd5a26ac6be30250cae878ad45
Jeff Humes <jeffitmind@gmail.com>
2024-11-01 14:57:16 -0500

    Retreive bean list from database and display in table working

--- frontend/src/components/JstockMain.js
+++ frontend/src/components/JstockMain.js
@@ -1,8 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 const JstockMain = () => {
   const [text, setText] = useState();
+  const [textList, setTextList] = useState([]);
 
   useEffect(() => {
     console.log("Entering UseEffect getString");
@@ -25,6 +33,7 @@ const JstockMain = () => {
       .get("/jstock/api/getStringFromDatabase")
       .then((response) => {
         console.log(response);
+        setTextList(response.data);
       })
       .catch((error) => {
         console.log(error);
@@ -34,7 +43,35 @@ const JstockMain = () => {
       });
   }, []);
 
-  return <div>{text}</div>;
+  return (
+    <>
+      <div>{text}</div>
+      <br></br>
+
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Text</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {textList.map((item) => (
+              <TableRow
+                key={item.stringName}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+                <TableCell component="th" scope="row">
+                  {item.stringName}
+                </TableCell>
+                <TableCell>{item.stringText}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
 };
 
 export default JstockMain;
